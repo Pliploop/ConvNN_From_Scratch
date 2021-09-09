acb8179f379c60822bb6a79aea37698693dbe0f2
Julien <julien.guinot@artefact.com>
2021-09-09 17:04:22 +0200

    working augmentations

--- notebooks/JulienNet.py
+++ notebooks/JulienNet.py
@@ -11,22 +11,67 @@ from sklearn.model_selection import train_test_split
 from sklearn.manifold import TSNE
 import pandas as pd
 import seaborn as sns
+from sklearn import metrics
+from matplotlib.colors import LogNorm, Normalize
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
+
+class Cifar10SearchDataset(torchvision.datasets.CIFAR10):
+    def __init__(self, root="~/data/cifar10", train=True, download=True, transform=None):
+        super().__init__(root=root, train=train, download=download, transform=transform)
+
+    def __getitem__(self, index):
+        image, label = self.data[index], self.targets[index]
+
+        if self.transform is not None:
+            transformed = self.transform(image=image)
+            image = transformed["image"]
+
+        return image, label
 
 class SetGenerator():
     def __init__(self, train_batch_size, test_batch_size) -> None:
-        self.basic_transforms = transforms.Compose(
-            [transforms.ToTensor(),
-             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+        self.basic_transforms = A.Compose(
+            [
+                A.OneOf([
+                    A.RGBShift(r_shift_limit=15, g_shift_limit=15,
+                               b_shift_limit=15, p=0.5),
+                    A.RandomBrightnessContrast(p=0.5), ], p=0.3),
+
+
+                A.OneOf([
+                    A.HorizontalFlip(p=0.6),
+                    A.RandomRotate90(p=0.6),
+                    A.VerticalFlip(p=0.3)
+                ], p=0.3),
+
+                A.OneOf([
+                    A.MotionBlur(p=0.6),
+                    A.OpticalDistortion(p=0.6),
+                    A.GaussNoise(p=0.3)
+                ], p=0.3),
+
+
+                A.Normalize(mean=(0.485, 0.456, 0.406),
+                            std=(0.229, 0.224, 0.225)),
+                ToTensorV2()
+            ]
+        )
+        # self.basic_transforms = transforms.Compose(
+        #     [transforms.ToTensor(),
+        #      transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])]
+
         self.train_batch_size = train_batch_size
         self.test_batch_size = test_batch_size
 
         self.val_size = 0.1
 
-        self.training_set = torchvision.datasets.CIFAR10(root='./data', train=True,
+        self.training_set = Cifar10SearchDataset(root='./data', train=True,
                                                          download=True, transform=self.basic_transforms)
 
-        self.test_set = torchvision.datasets.CIFAR10(root='./data', train=False,
+        self.test_set = Cifar10SearchDataset(root='./data', train=False,
                                                      download=True, transform=self.basic_transforms)
 
         self.classes = ['plane', 'car', 'bird', 'cat',
@@ -47,10 +92,9 @@ class SetGenerator():
         self.training_data_loader = torch.utils.data.DataLoader(self.train_split, batch_size=self.train_batch_size,
                                                                 shuffle=True, num_workers=2)
         self.val_data_loader = torch.utils.data.DataLoader(self.val_split, batch_size=self.train_batch_size,
-                                                                shuffle=True, num_workers=2)
+                                                           shuffle=True, num_workers=2)
         self.test_data_loader = torch.utils.data.DataLoader(self.test_set, batch_size=self.test_batch_size,
                                                             shuffle=False, num_workers=2)
-
 
     def imshow(self, img):
         img = img / 2 + 0.5     # unnormalize
@@ -77,7 +121,7 @@ def block(x):
 
 class JulienNet(nn.Module):
 
-    def __init__(self,dropout) -> None:
+    def __init__(self, dropout) -> None:
         super().__init__()
         self.conv1_1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
         self.conv1_2 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
@@ -90,7 +134,6 @@ class JulienNet(nn.Module):
         self.bn_2 = nn.BatchNorm2d(64)
         self.mp_2 = nn.MaxPool2d(2, 2)
         self.drop_2_2d = nn.Dropout2d(p=0.3)
-
 
         self.conv3_1 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
         self.conv3_2 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
@@ -142,7 +185,7 @@ class JulienNet(nn.Module):
 
 
 class JNetTrainer:
-    def __init__(self, SetGenerator, n_epochs,dropout,save_path,save=False) -> None:
+    def __init__(self, SetGenerator, n_epochs, dropout, save_path, save=False) -> None:
         self.SetGenerator = SetGenerator
         self.net = JulienNet(dropout=dropout)
         self.device = torch.device(
@@ -151,7 +194,7 @@ class JNetTrainer:
         self.net.to(self.device)
         self.criterion = nn.CrossEntropyLoss()
         self.optimizer = optim.SGD(
-            self.net.parameters(), lr=0.01, momentum=0.9,weight_decay=1e-4)
+            self.net.parameters(), lr=0.0001, momentum=0.9, weight_decay=1e-4)
         self.n_epochs = n_epochs
         self.train_loss_list = np.zeros(n_epochs)
         self.val_loss_list = np.zeros(n_epochs)
@@ -161,10 +204,10 @@ class JNetTrainer:
         self.val_loss_list[:] = np.NaN
         self.train_accuracy_list[:] = np.NaN
         self.val_accuracy_list[:] = np.NaN
-        self.fig,self.ax=plt.subplots(1,2)
-        self.save_path=save_path
+        self.fig, self.ax = plt.subplots(1, 2)
+        self.save_path = save_path
         self.save = save
-        
+
     def train(self):
         epochs = trange(self.n_epochs, leave=True)
         for epoch in epochs:  # loop over the dataset multiple times
@@ -195,99 +238,93 @@ class JNetTrainer:
             val_total = 0
             train_correct = 0
             val_correct = 0
-            
 
-            
             with torch.no_grad():
                 self.net.eval()
                 for data in self.SetGenerator.training_data_loader:
 
                     images, labels = data[0].to(
-                    self.device), data[1].to(self.device)
+                        self.device), data[1].to(self.device)
 
                     outputs = self.net(images)
-                    predicted=torch.argmax(outputs, dim = 1)
+                    predicted = torch.argmax(outputs, dim=1)
                     train_total += labels.size(0)
                     train_correct += (predicted == labels).sum().item()
 
-                self.train_accuracy_list[epoch] = 100 * train_correct / train_total
+                self.train_accuracy_list[epoch] = 100 * \
+                    train_correct / train_total
                 self.train_loss_list[epoch] = self.criterion(outputs, labels)
-                
+
                 for data in self.SetGenerator.val_data_loader:
 
                     images, labels = data[0].to(
-                    self.device), data[1].to(self.device)
+                        self.device), data[1].to(self.device)
 
                     outputs = self.net(images)
-                    predicted=torch.argmax(outputs, dim = 1)
+                    predicted = torch.argmax(outputs, dim=1)
                     val_total += labels.size(0)
                     val_correct += (predicted == labels).sum().item()
                 self.val_accuracy_list[epoch] = 100 * val_correct / val_total
                 self.val_loss_list[epoch] = self.criterion(outputs, labels)
-                self.net.train()   
-
-
-
+                self.net.train()
 
         print('Finished Training')
         if self.save:
             torch.save(self.net.state_dict(), self.save_path)
 
     def plot_metrics(self):
-        ax=self.ax
+        ax = self.ax
         self.fig.clear()
-        
-        ax[0].plot(self.train_loss_list,color='k')
-        ax[0].plot(self.val_loss_list,color='g')
-        ax[0].set(title="Loss",xlabel="n_epochs",ylabel="loss")
-        
-        ax[1].plot(self.train_accuracy_list,color='b')
-        ax[1].plot(self.val_accuracy_list,color='r')
-        ax[1].set(title="Accuracy",xlabel="n_epochs",ylabel="accuracy",ylim=[0,100])
+
+        ax[0].plot(self.train_loss_list, color='k')
+        ax[0].plot(self.val_loss_list, color='g')
+        ax[0].set(title="Loss", xlabel="n_epochs", ylabel="loss")
+
+        ax[1].plot(self.train_accuracy_list, color='b')
+        ax[1].plot(self.val_accuracy_list, color='r')
+        ax[1].set(title="Accuracy", xlabel="n_epochs",
+                  ylabel="accuracy", ylim=[0, 100])
 
         plt.show()
 
-    def load(self,path):
+    def load(self, path):
         self.net = JulienNet(True)
         self.net.load_state_dict(torch.load(path))
         self.net.to(self.device)
 
-    
-
-
-    def predict(self,img):
+    def predict(self, img):
         output = self.net(img)
         predicted_class = torch.argmax(output)
         predicted_class_name = self.SetGenerator.classes[predicted_class]
         return predicted_class_name
 
     def get_feature_reduction(self):
-        features = torch.empty(0,64)
-        classes = np.empty((0,1))
+        features = torch.empty(0, 64)
+        classes = np.empty((0, 1))
         activation = {}
+
         def get_activation(name):
             def hook(model, input, output):
                 activation[name] = output.detach()
             return hook
         self.net.fc_2.register_forward_hook(get_activation('fc2'))
         self.net.eval()
-        for data in self.SetGenerator.test_set:
+        for data in self.SetGenerator.train_split:
             # print(data)
 
-            img, labels = data[0].view(-1,3,32,32).to(
-            self.device), data[1]
+            img, labels = data[0].view(-1, 3, 32, 32).to(
+                self.device), data[1]
 
             outputs = self.net(img)
-            features=torch.cat([features,activation['fc2'].cpu()])
-            classes=np.append(classes,self.SetGenerator.classes[labels])
-            
+            features = torch.cat([features, activation['fc2'].cpu()])
+            classes = np.append(classes, self.SetGenerator.classes[labels])
 
         features_embed = TSNE(n_components=2).fit_transform(features)
         dim_reduction_data = pd.DataFrame()
-        dim_reduction_data['d1']=features_embed[:,0]
-        dim_reduction_data['d2']=features_embed[:,1]
-        dim_reduction_data['class']=classes
-        plt.figure(figsize=(16,10))
+        dim_reduction_data['d1'] = features_embed[:, 0]
+        dim_reduction_data['d2'] = features_embed[:, 1]
+        dim_reduction_data['class'] = classes
+        fig = plt.figure(figsize=(16, 10))
         sns.scatterplot(
             x="d1", y="d2",
             hue="class",
@@ -296,4 +333,43 @@ class JNetTrainer:
             legend="full",
             alpha=0.3
         )
-        plt.show()
+
+        return fig
+
+    def get_test_predictions(self):
+        predictions = np.empty((0))
+        gt = np.empty((0))
+        with torch.no_grad():
+            self.net.eval()
+            for data in self.SetGenerator.test_data_loader:
+                images, labels = data[0].to(
+                    self.device), data[1].to(self.device)
+
+                outputs = self.net(images)
+                predicted = torch.argmax(outputs, dim=1)
+                predictions = np.concatenate((predictions, predicted.cpu()))
+                gt = np.concatenate((gt, labels.cpu().numpy()))
+            self.net.train()
+        return predictions, gt
+
+    def get_metrics(self):
+        y_pred, y = self.get_test_predictions()
+        classes = self.SetGenerator.classes
+        global_acc = metrics.accuracy_score(y_pred, y)
+        for k in range(len(classes)):
+            idx = np.argwhere(y == k)
+            class_y = y[idx] == k
+            class_y_pred = y_pred[idx] == k
+            class_acc = metrics.accuracy_score(class_y, class_y_pred)
+            class_precision = metrics.precision_score(class_y, class_y_pred)
+            class_recall = metrics.recall_score(class_y, class_y_pred)
+            class_F1 = metrics.f1_score(class_y, class_y_pred)
+            row = [classes[k], class_acc,
+                   class_precision, class_recall, class_F1]
+            print(row)
+        print(global_acc)
+        confusion_matrix = metrics.confusion_matrix(y_pred, y)
+        fig, ax = plt.subplots(figsize=(15, 15))
+        sns.heatmap(confusion_matrix/np.sum(confusion_matrix), annot=True,
+                    cmap='magma', xticklabels=classes, yticklabels=classes, norm=LogNorm())
+        return fig
